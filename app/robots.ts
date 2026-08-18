1bf8a78f663b950fa83e4667c862553ac18ed500
Andrew Smith <asmit119@me.com>
2026-08-18 18:14:35 -0400

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://mcu-doomsday-clock.vercel.app/sitemap.xml",
+    sitemap: "https://ninefold-chord-lightfall.vercel.app/sitemap.xml",
   };
 }
